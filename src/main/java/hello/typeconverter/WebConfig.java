dbb0421ff9a29e448e184f019f230cd4700c2fe0
dohyun97 <dohyun647@gmail.com>
2023-04-16 14:51:32 -0400

    register formatter in WebConfig

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new IntegerToStringConverter());
-        registry.addConverter(new StringToIntegerConverter());
+        //MyNumberFormatter랑 기능이 겹쳐. 우선순위는 컨버터가 먼저여서 주석처리
+//        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToIntegerConverter());
+
         registry.addConverter(new IpPortToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
